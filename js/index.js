aca8bf5b6c0efb0635a20706c8f1298997d52800
Charlie <traceurling@gmail.com>
2020-06-01 14:18:03 -0700

    mvp

--- js/index.js
+++ js/index.js
@@ -37,6 +37,29 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+document.querySelectorAll('a')[0].textContent = siteContent['nav']['nav-item-1']
+document.querySelectorAll('a')[1].textContent = siteContent['nav']['nav-item-2']
+document.querySelectorAll('a')[2].textContent = siteContent['nav']['nav-item-3']
+document.querySelectorAll('a')[3].textContent = siteContent['nav']['nav-item-4']
+document.querySelectorAll('a')[4].textContent = siteContent['nav']['nav-item-5']
+document.querySelectorAll('a')[5].textContent = siteContent['nav']['nav-item-6']
+document.getElementById('logo-img').setAttribute('src', siteContent['nav']['img-src'])
+document.querySelector('h1').textContent = siteContent['cta']['h1']
+document.querySelector('button').textContent = siteContent['cta']['button']
+document.getElementById('cta-img').setAttribute('src', siteContent['cta']['img-src'])
+document.querySelectorAll('h4')[0].textContent = siteContent['main-content']['features-h4']
+document.querySelectorAll('p')[0].textContent = siteContent['main-content']['features-content']
+document.querySelectorAll('h4')[1].textContent = siteContent['main-content']['about-h4']
+document.querySelectorAll('p')[1].textContent = siteContent['main-content']['about-content']
+document.getElementById('middle-img').setAttribute('src', siteContent['main-content']['middle-img-src'])
+document.querySelectorAll('h4')[2].textContent = siteContent['main-content']['services-h4']
+document.querySelectorAll('p')[2].textContent = siteContent['main-content']['services-content']
+document.querySelectorAll('h4')[3].textContent = siteContent['main-content']['product-h4']
+document.querySelectorAll('p')[3].textContent = siteContent['main-content']['product-content']
+document.querySelectorAll('h4')[4].textContent = siteContent['main-content']['vision-h4']
+document.querySelectorAll('p')[4].textContent = siteContent['main-content']['vision-content']
+document.querySelectorAll('h4')[5].textContent = siteContent['contact']['contact-h4']
+document.querySelectorAll('p')[5].textContent = siteContent['contact']['address']
+document.querySelectorAll('p')[6].textContent = siteContent['contact']['phone']
+document.querySelectorAll('p')[7].textContent = siteContent['contact']['email']
+document.querySelectorAll('p')[8].textContent = siteContent['footer']['copyright']
